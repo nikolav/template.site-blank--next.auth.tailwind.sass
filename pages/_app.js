9e575670402fd1f9ab652fd83e5e7ce9d9ead701
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-06-20 14:13:35 +0200

    adds global .scss; globals.scss

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import { SessionProvider } from "next-auth/react";
 
 import "../styles/reset.css";
 import "../styles/build.css";
-
+import "../styles/globals.css";
+//
+////
+////
 function MyApp({ Component, pageProps: { session, ...restPageProps } }) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={session} refetchInterval={0}>
       <Component {...restPageProps} />
     </SessionProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,33 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import modcss from "../styles/Index.module.css";
 
 export default function Index() {
-  const { data: session, status: authStatus } = useSession();
+  const { data: auth, status: authStatus } = useSession();
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>next-auth</title>
         <meta name="description" content="NextApp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="prose">
         <h1 className="text-slate-500 uppercase font-bold">hello</h1>
       </div>
-      {session ? (
+      {auth ? (
         <button
-          className="bg-slate-700 p-2 px-4 text-white rounded-full"
-          onClick={signOut}
+          className="bg-slate-500 text-sm hover:bg-slate-600 p-1 px-4 text-white rounded-full"
+          onClick={() => signOut({ redirect: false, callbackUrl: "/a-page" })}
         >
           logout
         </button>
       ) : (
         <button
-          className="bg-slate-700 p-2 px-4 text-white rounded-full"
-          onClick={signIn}
+          className="bg-slate-500 text-sm hover:bg-slate-600 p-1 px-4 text-white rounded-full"
+          onClick={() => signIn("github")}
         >
-          login
+          gh.login
         </button>
       )}
-      <pre className="text-xs">{JSON.stringify(session, null, 2)}</pre>
+      <pre className="text-xs">{JSON.stringify(auth, null, 2)}</pre>
       <pre className="text-xs">{JSON.stringify(authStatus, null, 2)}</pre>
     </>
   );
