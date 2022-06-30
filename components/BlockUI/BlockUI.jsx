fbe19648656dd7269f191d77ded140d89350b850
nikolav <admin@nikolav.rs>
2022-06-30 23:49:25 +0100

    #2

--- components/BlockUI/BlockUI.jsx
+++ components/BlockUI/BlockUI.jsx
@@ -1,32 +1,34 @@
 import React, { useRef, useEffect } from "react";
 import PortalOverlays from "../PortalOverlays";
 import { motion, AnimatePresence } from "framer-motion";
-import { useFlags, FLAG_BLOKUI } from "../../app/store";
 ////
 ////
-const BlockUI = ({ className = "", children }) => {
-  const flags = useFlags();
-  const isActive = true === flags(FLAG_BLOKUI);
-  const refBlocked = useRef();
+const BlockUI = ({ isActive, className = "", children, ...rest }) => {
+  const refBlock = useRef();
   useEffect(() => {
-    if (isActive) refBlocked.current.focus();
+    if (isActive) refBlock.current.focus();
   }, [isActive]);
   //
   return (
     <PortalOverlays end={true}>
       <AnimatePresence>
-        {true === isActive ? (
+        {isActive ? (
           <section
-            ref={refBlocked}
+            ref={refBlock}
             style={{ zIndex: 9999 }}
             className={`m-0 p-0 bg-white/10 backdrop-blur-sm flex items-center justify-center fixed inset-0 ${className}`}
+            {...rest}
           >
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1, transition: { duration: 0.2 } }}
               exit={{ opacity: 0, transition: { duration: 0.1 } }}
             >
-              {children ?? <strong className="text-5xl inline-block blockuiSpinner">⏳</strong>}
+              {children ?? (
+                <strong className="text-5xl inline-block blockuiSpinner">
+                  ⏳
+                </strong>
+              )}
             </motion.div>
           </section>
         ) : null}
