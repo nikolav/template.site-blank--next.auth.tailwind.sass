8308f1af6e3afcbb9394d5b084cfc9cf73734fb6
nikolav <admin@nikolav.rs>
2022-07-08 10:22:47 +0100

    +icons

---
+++ components/icons/index.js
@@ -0,0 +1,125 @@
+import iconSearch from "../../app/etc/icon-search-primary.svg";
+import iconFactory from "../../app/etc/icon-factory.svg";
+import iconGrape from "../../app/etc/icon-grape.svg";
+import iconDinarCyr from "../../app/etc/icon-dinar-cyr.svg";
+//
+import { HiOutlineBookOpen } from "react-icons/hi";
+import { IoMdPower } from "react-icons/io";
+import { TiArrowLeftThick } from "react-icons/ti";
+import { CgFormatItalic } from "react-icons/cg";
+import { ImQuotesLeft } from "react-icons/im";
+import { GoListUnordered, GoListOrdered } from "react-icons/go";
+import {
+  FaStar,
+  FaHome,
+  FaFacebookF,
+  FaViber,
+  FaAlignLeft,
+  FaAlignCenter,
+  FaAlignRight,
+  FaAlignJustify,
+  FaRegComments,
+  FaExclamationTriangle,
+  FaRegSave,
+  FaFacebook,
+  FaGithubAlt,
+} from "react-icons/fa";
+import { IoHelp } from "react-icons/io5";
+import { FiCamera, FiShare2 } from "react-icons/fi";
+import { BsStarFill, BsPlayFill } from "react-icons/bs";
+import { RiGithubLine, RiTwitterLine, RiMessengerLine } from "react-icons/ri";
+import {
+  BiShow,
+  BiUserCircle,
+  BiBold,
+  BiCloudUpload,
+  BiImage,
+  BiRefresh,
+} from "react-icons/bi";
+import {
+  MdDeleteOutline,
+  MdCreate,
+  MdClose,
+  MdOutlineMail,
+  MdOutlineAddPhotoAlternate,
+  MdOutlinePostAdd,
+  MdOutlineEditNote,
+} from "react-icons/md";
+import {
+  AiOutlineUnderline,
+  AiOutlineWhatsApp,
+  AiOutlineLink,
+  AiOutlineCloudSync,
+  //
+  AiOutlineTag,
+  AiOutlineCalendar,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+import {
+  RiCloseCircleFill,
+  RiCheckboxBlankCircleLine,
+  RiCheckboxCircleFill,
+} from "react-icons/ri";
+import { FiSend } from "react-icons/fi";
+import { FcGoogle } from "react-icons/fc";
+////
+////
+export {
+  AiFillTwitterCircle,
+  AiOutlineCalendar,
+  AiOutlineCloudSync,
+  AiOutlineLink,
+  AiOutlineTag,
+  AiOutlineUnderline,
+  AiOutlineWhatsApp,
+  BiBold,
+  BiCloudUpload,
+  BiImage,
+  BiRefresh,
+  BiShow,
+  BiUserCircle,
+  BsPlayFill,
+  BsStarFill,
+  CgFormatItalic,
+  FaAlignCenter,
+  FaAlignJustify,
+  FaAlignLeft,
+  FaAlignRight,
+  FaExclamationTriangle,
+  FaFacebook,
+  FaFacebookF,
+  FaGithubAlt,
+  FaHome,
+  FaRegComments,
+  FaRegSave,
+  FaStar,
+  FaViber,
+  FcGoogle,
+  FiCamera,
+  FiSend,
+  FiShare2,
+  GoListOrdered,
+  GoListUnordered,
+  HiOutlineBookOpen,
+  iconDinarCyr,
+  iconFactory,
+  iconGrape,
+  iconSearch,
+  ImQuotesLeft,
+  IoHelp,
+  IoMdPower,
+  MdClose,
+  MdCreate,
+  MdDeleteOutline,
+  MdOutlineAddPhotoAlternate,
+  MdOutlineEditNote,
+  MdOutlineMail,
+  MdOutlinePostAdd,
+  RiCheckboxBlankCircleLine,
+  RiCheckboxCircleFill,
+  RiCloseCircleFill,
+  RiGithubLine,
+  RiMessengerLine,
+  RiTwitterLine,
+  TiArrowLeftThick,
+};
